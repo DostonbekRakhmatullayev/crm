9e9e0f46806a5eb57c899e4a382a08e3ca854bc2
DostonbekRakhmatullayev <rahmatullayivdoston@gmail.com>
2023-09-03 17:40:23 +0500

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './config';
 import { typeOrmAsyncConfig } from './config/database';
-import { CategoriesModule } from './modules/categories/categories.module';
-import { MonthlyModule } from './modules/monthly/monthly.module';
-import { ProvidersModule } from './modules/provinces/provices.module';
 import { AdminModule } from './modules/Admin/admin.module';
 import { WorkersModule } from './modules/workers/workers.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { ProvidersModule } from './modules/provinces/provices.module';
+import { MonthlyModule } from './modules/monthly/monthly.module';
 
 @Module({
   imports: [
